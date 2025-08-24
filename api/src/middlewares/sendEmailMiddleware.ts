44a3c4402b5fc994f7e0876ba6964a6faa234e46
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-08-24 01:38:31 -0300

    fix: change url in email

--- api/src/middlewares/sendEmailMiddleware.ts
+++ api/src/middlewares/sendEmailMiddleware.ts
@@ -21,7 +21,7 @@ export const sendEmailMiddleware = async (
 
     const html = generateInviteEmail(
       travel.destination,
-      `https://intellitrip-two.vercel.app/travel/invite/${travelId}`
+      `https://intellitrip-two.vercel.app/travels/invite/${travelId}`
     );
 
     await sendInviteEmail(recieverEmail, "Convite para uma viagem", html);
