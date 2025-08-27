36b88b74ce003894be1cc4195b330fc8f3b538a0
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-08-27 20:51:58 -0300

    fix: cors origin

--- api/src/app.ts
+++ api/src/app.ts
@@ -9,8 +9,8 @@ import geminiRoutes from "./routes/geminiRoutes";
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3001" }));
-// app.use(cors({ origin: "https://intellitrip-two.vercel.app" }));
+//app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors({ origin: "https://intellitrip-two.vercel.app" }));
 
 app.use("/travel", travelRoutes);
 app.use("/activity", activityRoutes);
